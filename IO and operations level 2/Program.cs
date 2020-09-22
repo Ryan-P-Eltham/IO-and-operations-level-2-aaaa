faf975d01cdab858368f9ee8b1ba717fc7a79640
Ryan Perry <K039290@eltham-college.org.uk>
2020-09-22 21:30:06 +0100

    Add project files.

---
+++ IO and operations level 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_and_operations_level_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First value here");
+            string num1 = Console.ReadLine();
+                Console.WriteLine("Second value here");
+                string num2 = Console.ReadLine();
+                int trunum1 = Convert.ToInt32(num1);
+                int trunum2 = Convert.ToInt32(num2);
+                Console.WriteLine("Operation here");
+                string operation = Console.ReadLine();
+            string multiply = "*";
+            string divide = "/";
+            string add = "+";
+            string subtract = "-";
+            if (operation == multiply)
+            {
+                Console.WriteLine(trunum1 * trunum2);
+            }
+            else if (operation == divide)
+            {
+                Console.WriteLine(trunum1 / trunum2);
+            }
+            else if (operation == add)
+            {
+                Console.WriteLine(trunum1 + trunum2);
+            }
+            else if (operation == subtract)
+            {
+                Console.WriteLine(trunum1 - trunum2);
+                    }
+            else
+            {
+                Console.WriteLine(">:(");
+                Console.WriteLine("Why");
+                }
+            Console.ReadLine();
+            
+        }
+    }
+}
